f9e642951b18ae7e4ca1234b47c7c56ba5ae26c8
AnMiRomanov <am-romanov@mail.ru>
2022-03-08 17:59:00 +0500

    Homework2 solution

--- src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
+++ src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
@@ -1,11 +1,15 @@
 package ru.digitalhabbits.homework2;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 
 import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.*;
 
 import static java.lang.Runtime.getRuntime;
 import static java.nio.charset.Charset.defaultCharset;
@@ -14,26 +18,51 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class FileProcessor {
     private static final Logger logger = getLogger(FileProcessor.class);
     public static final int CHUNK_SIZE = 2 * getRuntime().availableProcessors();
+    final ExecutorService executorService = Executors.newCachedThreadPool();
+    Exchanger<String> exchanger = new Exchanger<>();
 
     public void process(@Nonnull String processingFileName, @Nonnull String resultFileName) {
         checkFileExists(processingFileName);
 
         final File file = new File(processingFileName);
-        // TODO: NotImplemented: запускаем FileWriter в отдельном потоке
+        List<String> tmpList = new ArrayList<>();
+        List<Callable<Pair<String, Integer>>> callableList = new ArrayList<>();
+        List<Future<Pair<String, Integer>>> futureList = new ArrayList<>();
+
+        Thread fileWriterThread = new Thread(new FileWriter(exchanger, resultFileName));
+        fileWriterThread.start();
 
         try (final Scanner scanner = new Scanner(file, defaultCharset())) {
             while (scanner.hasNext()) {
-                // TODO: NotImplemented: вычитываем CHUNK_SIZE строк для параллельной обработки
+                tmpList.add(scanner.nextLine());
+                //Согласно условию в коллекции можно хранить не больше CHUNK_SIZE обрабатываемых строк
+                if (tmpList.size() == CHUNK_SIZE){
+                    for (String str : tmpList){
+                        Callable<Pair<String, Integer>> callable = () -> new LineCounterProcessor().process(str);
+                        callableList.add(callable);
+                    }
 
-                // TODO: NotImplemented: обрабатывать строку с помощью LineProcessor. Каждый поток обрабатывает свою строку.
+                    futureList = executorService.invokeAll(callableList);
+                    tmpList.clear();
+                    callableList.clear();
 
-                // TODO: NotImplemented: добавить обработанные данные в результирующий файл
+                    for (Future<Pair<String, Integer>> future : futureList){
+                        Pair<String, Integer> pair = future.get();
+                        String text = pair.getLeft();
+                        String countSymbols = pair.getRight().toString();
+                        String resultString = text + " " + countSymbols;
+                        exchanger.exchange(resultString);
+                    }
+
+                    futureList.clear();
+                }
             }
-        } catch (IOException exception) {
+        } catch (IOException | InterruptedException | ExecutionException exception) {
             logger.error("", exception);
         }
 
-        // TODO: NotImplemented: остановить поток writerThread
+        fileWriterThread.interrupt();
+        executorService.shutdown();
 
         logger.info("Finish main thread {}", Thread.currentThread().getName());
     }
--- src/main/java/ru/digitalhabbits/homework2/FileWriter.java
+++ src/main/java/ru/digitalhabbits/homework2/FileWriter.java
@@ -2,6 +2,12 @@ package ru.digitalhabbits.homework2;
 
 import org.slf4j.Logger;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.Exchanger;
+
 import static java.lang.Thread.currentThread;
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -9,9 +15,25 @@ public class FileWriter
         implements Runnable {
     private static final Logger logger = getLogger(FileWriter.class);
 
+    Exchanger<String> exchanger;
+    File resultFile;
+
+    public FileWriter(Exchanger<String> exchanger, String resultFileName) {
+        this.exchanger = exchanger;
+        this.resultFile = new File(resultFileName);
+    }
+
     @Override
     public void run() {
         logger.info("Started writer thread {}", currentThread().getName());
+        while (!Thread.currentThread().isInterrupted()){
+            try {
+                String exchangeString = exchanger.exchange(null) + System.lineSeparator();
+                Files.writeString(resultFile.toPath(), exchangeString, StandardOpenOption.APPEND);
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         logger.info("Finish writer thread {}", currentThread().getName());
     }
